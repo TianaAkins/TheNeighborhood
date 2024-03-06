a640808d104e4074ccc85b5f112d8f0b2d0d76b1
Tiana Akins <tiana.aakins95@gmail.com>
2024-03-06 12:37:08 -0600

    Initial Commit

---
+++ TheNeighborHood/TheNeighborHoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheNeighborHoodApp.swift
+//  TheNeighborHood
+//
+//  Created by Tiana Akins on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TheNeighborHoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
